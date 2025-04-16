2bce51070f86221c30f7265ae8b542e9b7fe6efa
GuiJanunzzi <guijanunzzi7@gmail.com>
2025-04-15 22:49:40 -0300

    Filters

--- money-flow-api/src/main/java/br/com/fiap/money_flow_api/controller/TransactionController.java
+++ money-flow-api/src/main/java/br/com/fiap/money_flow_api/controller/TransactionController.java
@@ -1,14 +1,21 @@
 package br.com.fiap.money_flow_api.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.money_flow_api.model.Transaction;
+import br.com.fiap.money_flow_api.model.TransactionFilter;
 import br.com.fiap.money_flow_api.repository.TransactionRepository;
+import br.com.fiap.money_flow_api.specification.TransactionSpecification;
 
 @RestController
 @RequestMapping("transactions")
@@ -18,8 +25,30 @@ public class TransactionController {
     private TransactionRepository repository;
 
     @GetMapping
-    public List<Transaction> index(){
-        return repository.findAll();
+    public Page<Transaction> index(
+        TransactionFilter filter,
+        @PageableDefault(size = 10, sort = "date", direction = Direction.DESC) Pageable pageable){
+
+            // if (filter.description() != null) {
+            //     return repository.findByDescriptionContainingIgnoringCase(filter.description(), pageable);
+            // }
+
+            // ------------------------------------------------------------------------------------------------------------
+            // var probe = Transaction.builder()
+            //     .description(filter.description())
+            //     .date(filter.startDate())
+            //     .build();
+
+            // var matcher = ExampleMatcher
+            //     .matchingAll()
+            //     .withIgnoreCase()
+            //     .withStringMatcher(StringMatcher.CONTAINING);
+
+            // var example = Example.of(probe, matcher);
+
+            var specification = TransactionSpecification.withFilter(filter);
+            
+            return repository.findAll(specification, pageable);
     }
     
 }
