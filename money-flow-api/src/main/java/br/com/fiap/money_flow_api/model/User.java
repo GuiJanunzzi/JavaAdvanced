7f3bc84dd0560d36e57669a06a0cf33755569b9e
GuiJanunzzi <guijanunzzi7@gmail.com>
2025-05-05 23:15:05 -0300

    Refatorando a entidade de Usuario para usar EnumType em Role

--- money-flow-api/src/main/java/br/com/fiap/money_flow_api/model/User.java
+++ money-flow-api/src/main/java/br/com/fiap/money_flow_api/model/User.java
@@ -8,6 +8,8 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -37,7 +39,7 @@ public class User {
     @NotBlank
     private String password;
  
-    @Pattern(regexp = "(ADMIN|USER)")
+    @Enumerated(EnumType.STRING)
     private UserRole role;
  
     @Override
