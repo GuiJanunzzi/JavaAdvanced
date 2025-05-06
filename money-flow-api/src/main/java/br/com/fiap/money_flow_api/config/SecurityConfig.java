7c71a51a269a0c9e37ba5e59d36f183264179264
GuiJanunzzi <guijanunzzi7@gmail.com>
2025-05-05 23:39:21 -0300

    Aula 05-05 Adicionado Autanticação JWT com AuthFilter e TokenService

--- money-flow-api/src/main/java/br/com/fiap/money_flow_api/config/SecurityConfig.java
+++ money-flow-api/src/main/java/br/com/fiap/money_flow_api/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.fiap.money_flow_api.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -14,22 +15,25 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    private AuthFilter authFilter;
+
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth
-                    .requestMatchers(HttpMethod.DELETE, "/users/**").hasRole("ADMIN")
-                    .requestMatchers("/transactions/**").hasRole("ADMIN")
-                    .requestMatchers("/transactions/**").hasRole("USER")
-                    .anyRequest().authenticated()
-                )
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .requestMatchers("/transactions/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/users/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login/**").permitAll()
+                        .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+                .csrf(csrf -> csrf.disable())
                 .build();
     }
 
