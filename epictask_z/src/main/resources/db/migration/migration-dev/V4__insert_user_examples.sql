940005f2dbd621d8d11a6e0c546da6f6ab2d3cd6
GuiJanunzzi <guijanunzzi7@gmail.com>
2025-09-08 22:45:55 -0300

    Preparando para deploy

--- epictask_z/src/main/resources/db/migration/migration-dev/V4__insert_user_examples.sql
+++ epictask_z/src/main/resources/db/migration/migration-dev/V4__insert_user_examples.sql
@@ -1,3 +1,4 @@
-INSERT INTO  epicuser(name, email, avatar_url) VALUES
-('Ana','ana@fiap.com.br','https://github.com/ana.png'),
-('Bruno','bruno@fiap.com.br','https://github.com/bruno.png')
+INSERT INTO epicuser (name, email, avatar_url) VALUES
+('Ana', 'ana@fiap.com.br', 'https://github.com/ana.png'),
+('Bruno', 'bruno@fiap.com.br', 'https://github.com/bruno.png')
+;
